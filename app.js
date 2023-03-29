0ebd3449781044e76ab6c24270196c6308c77aec
sailajalakkakula <123012535+sailajalakkakula@users.noreply.github.com>
2023-03-29 10:16:59 -0500

    change the location of file

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use('/users', usersRouter);
 
 app.use('/mydata', mydataRouter);
 
+app.use('/compute', computeRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
